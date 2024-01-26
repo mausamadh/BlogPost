22a77fe721764426c79fdc3f8ac407aa8534c2b8
Mausam Adhikari <mausamadhikari20@gmail.com>
2024-01-26 12:33:05 +0545

    refactor crud services

--- src/crud/repository/repository.py
+++ src/crud/repository/repository.py
@@ -1,36 +1,35 @@
-
 from fastapi import HTTPException
 from src.entrypoint.database import user
 
+
 class InMemoryRep:
     def __init__(self):
-        self.repo=user
+        self.repo = user
 
-    def add(self,item):
+    def add(self, item):
         self.repo.append(item)
         return "added successfully"
 
-    def update(self,index,change):
-        if 0<=index<len(self.repo):
-            existing=self.repo[index]
+    def update(self, index, change):
+        if 0 <= index < len(self.repo):
+            existing = self.repo[index]
         else:
-            raise HTTPException(status_code=404,detail="user not found")
-            
-        for key,value in change.items():
-            setattr(existing,key,value)
-        
-        
-        self.repo[index]=existing
+            raise HTTPException(status_code=404, detail="user not found")
+
+        for key, value in change.items():
+            setattr(existing, key, value)
+
+        self.repo[index] = existing
         return "updated successfully"
-    
-    def get(self,index):
+
+    def get(self, index):
         return self.repo[index]
-    
+
     def getAll(self):
-        
         return self.repo
-    
-    def delete(self,index):
+
+    def delete(self, index):
         return self.repo.pop(index)
 
-repo=InMemoryRep
+
+repo = InMemoryRep
--- src/crud/router/router.py
+++ src/crud/router/router.py
@@ -1,32 +1,32 @@
-from fastapi import APIRouter,status
+from fastapi import APIRouter, status
 from src.crud.service.service import service
 from src.crud.model.model import User
 
-crud=APIRouter(prefix="/api/v1/crud",tags=["CRUD"])
+crud = APIRouter(prefix="/api/v1/crud", tags=["CRUD"])
 
 
-
-@crud.get("/get",status_code=status.HTTP_200_OK)
-async def crud_get(index:int):
-    
+@crud.get("/get", status_code=status.HTTP_200_OK)
+async def crud_get(index: int):
     return service().get(index=index)
-@crud.get("/getAll",status_code=status.HTTP_200_OK)
+
+
+@crud.get("/getAll", status_code=status.HTTP_200_OK)
 async def crud_getAll():
-    
     return service().getAll()
 
-@crud.post("/add",status_code=status.HTTP_200_OK)
-async def crud_post(data:User):
+
+@crud.post("/add", status_code=status.HTTP_200_OK)
+async def crud_post(data: User):
     return service().add(data=data)
 
 
-@crud.put("/update",status_code=status.HTTP_200_OK)
-async def crud_put(index:int,data:User):
-    updateUser=data.model_dump(exclude_unset=True)
-           
-    return service().update(index=index,data=updateUser)
+@crud.put("/update", status_code=status.HTTP_200_OK)
+async def crud_put(index: int, data: User):
+    updateUser = data.model_dump(exclude_unset=True)
+
+    return service().update(index=index, data=updateUser)
 
 
-@crud.delete("/delete",status_code=status.HTTP_200_OK)
-async def crud_delete(index:int):
+@crud.delete("/delete", status_code=status.HTTP_200_OK)
+async def crud_delete(index: int):
     return service().delete(index=index)
